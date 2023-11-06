0ff519c6461ce053e82dbeeb9e9291f39376ff08
sree-deepika <92073318+sree-deepika@users.noreply.github.com>
2023-11-06 18:27:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,79 @@
+<html>
+<title>Online Banking System</title>
+<head>
+   <link rel="stylesheet" type="text/css" href="css/index.css">
+   <link rel="shortcut icon" href="img/chase.jpg">
+    </head>
+ 
+<body>
+<?php include'header.php'?>
+<div class="index_container">
+    <div class="slider">
+    <div class="slideimg">
+    <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        <img src="img/onbnk.jpg">
+        </div>
+    </div><br>
+    <div class="newsroom">
+        
+        <marquee class="marquee_news" scrolldelay="20"><p class="newsfeed">
+        WELCOME TO BANK OF AMRITA 
+        </marquee>
+    </div><br><br>
+   
+
+    
+    <div class="news_events">
+        <h4>Tips | Updates | Events</h4><br>
+        <ul>
+            <p>RBI withdraws Rs 2,000 notes from circulation, asks all to exchange or deposit them by Sept 30</p> <br>
+            <p>Government tightens rules on global credit card transactions by Indian travellers abroad.</p>
+            <br>
+        </ul>
+    </div>
+    
+
+    <div class="online_services">
+        <h4>Online Services</h4>
+        <ul>
+            <a href="customer_reg_form.php"><li>Open Account</li></a>
+            <a href="debit_card_form.php"><li>Apply Debit Card</li></a><br>
+            <a href="#" id="ebanking" ><li><div class="ebanking">Internet Banking
+                <div class = "ebanking_options">
+                <ul>
+                    <a href="customer_login.php"><li>Login </li></a>
+                    <a href="ebanking_reg_form.php"><li>Register</li></a>
+                </ul>
+            </div>
+        </div>
+    </li></a>
+            <a href="fund_transfer.php"><li>Fund Transfer</li></a><br>
+        </ul>
+   
+</div>
+
+        <div id="aboutus" class="about"><span>About Us</span><br><br>
+            <p>Bank of Amrita is a renowned financial institution that has been serving customers with dedication and integrity for over 2 years. With a commitment to excellence and a customer-centric approach, Bank of Amrita has established itself as a trusted name in the banking industry. This article delves into the history, vision, mission, values, products, and services offered by Bank of Amrita, showcasing its unwavering commitment to empowering financial growth and building trust.
+
+Bank of Amrita was founded in 2021 with the vision of providing accessible and reliable financial services to individuals and businesses alike. Since its inception, the bank has grown steadily, expanding its reach and offering a comprehensive range of banking solutions. Throughout its history, Bank of Amrita has focused on adapting to the changing needs of its customers and has consistently upheld its core values of integrity, trust, and customer satisfaction.
+
+</p>
+        </div>
+    
+    <div class="disclaimer">
+    <span>Disclaimer !!</spasn><br><br>
+        <p>Our bank does not ask for the details of your account/PIN/password. Therefore any one pretending to be asking you for information from the bank/technical team may be fraudulent entities, so please beware.</p>
+        <p>You should know how to operate net transactions and if you are not familiar you may refrain from doing so. You may seek bank's guidance in this regard. Bank is not responsible for online transactions going wrong.</p>
+        <p>We shall also not be responsible for wrong transactions and wanton disclosure of details by you. Viewing option and transaction option on the net are different. You may exercise your option diligently.</p>
+    </div>
+
+
+    </div>
+    
+<?php include 'footer.php';?>
+</body>
+
+</html>
